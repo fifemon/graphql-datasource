327ddae5a6de50eb0e895711ef5220fa60584309
ryan <ryantxu@gmail.com>
2019-09-19 10:37:43 -0700

    use constant

--- src/QueryEditor.tsx
+++ src/QueryEditor.tsx
@@ -1,8 +1,11 @@
+import defaults from 'lodash/defaults';
+
 import React, { PureComponent, ChangeEvent } from 'react';
 import { FormField, QueryEditorProps } from '@grafana/ui';
-import { DataSource, Query, Options } from './DataSource';
+import { DataSource } from './DataSource';
+import { MyQuery, MyDataSourceOptions, defaultQuery } from './types';
 
-type Props = QueryEditorProps<DataSource, Query, Options>;
+type Props = QueryEditorProps<DataSource, MyQuery, MyDataSourceOptions>;
 
 interface State {}
 
@@ -14,13 +17,20 @@ export class QueryEditor extends PureComponent<Props, State> {
     onChange({ ...query, queryText: event.target.value });
   };
 
+  onConstantChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { onChange, query, onRunQuery } = this.props;
+    onChange({ ...query, constant: parseFloat(event.target.value) });
+    onRunQuery(); // executes the query
+  };
+
   render() {
-    const { query } = this.props;
-    const { queryText } = query;
+    const query = defaults(this.props.query, defaultQuery);
+    const { queryText, constant } = query;
 
     return (
       <div className="gf-form">
-        <FormField width={24} value={queryText || ''} onChange={this.onQueryTextChange} label="Query"></FormField>
+        <FormField width={4} value={constant} onChange={this.onConstantChange} label="Constant" type="number" step="0.1"></FormField>
+        <FormField labelWidth={8} value={queryText || ''} onChange={this.onQueryTextChange} label="Query Text" tooltip="Not used yet"></FormField>
       </div>
     );
   }
