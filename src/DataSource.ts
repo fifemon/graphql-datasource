e03cc1ae75b9bb62364eaa75387ddd9f40d18a5c
Kevin Retzke <kretzke@fnal.gov>
2021-03-09 16:29:00 +0000

    upgrade deps @grafana 7.4.3

--- src/DataSource.ts
+++ src/DataSource.ts
@@ -139,7 +139,7 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
 
   async query(options: DataQueryRequest<MyQuery>): Promise<DataQueryResponse> {
     return Promise.all(
-      options.targets.map(target => {
+      options.targets.map((target) => {
         return this.createQuery(defaults(target, defaultQuery), options.range, options.scopedVars);
       })
     ).then((results: any) => {
@@ -322,7 +322,7 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
 
   getVariables() {
     const variables: { [id: string]: TextValuePair } = {};
-    Object.values(getTemplateSrv().getVariables()).forEach(variable => {
+    Object.values(getTemplateSrv().getVariables()).forEach((variable) => {
       if (!supportedVariableTypes.includes(variable.type)) {
         console.warn(`Variable of type "${variable.type}" is not supported`);
 
@@ -334,7 +334,7 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
       let variableValue = supportedVariable.current.value;
       if (variableValue === '$__all' || isEqual(variableValue, ['$__all'])) {
         if (supportedVariable.allValue === null || supportedVariable.allValue === '') {
-          variableValue = supportedVariable.options.slice(1).map(textValuePair => textValuePair.value);
+          variableValue = supportedVariable.options.slice(1).map((textValuePair) => textValuePair.value);
         } else {
           variableValue = supportedVariable.allValue;
         }
